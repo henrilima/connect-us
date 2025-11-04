d4f465f215c7b84ac49d9a5e805edc1a87734477
Henrique Lima <henrilima.contactme@gmail.com>
2025-11-04 16:55:17 -0300

    feat: define o Brasil como padrão no pacote de internacionalização

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:connect/provider/auth_provider.dart';
 import 'package:connect/theme/app_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await initializeDateFormatting('pt_BR', null);
+  Intl.defaultLocale = 'pt_BR';
 
   // await DatabaseService().createRelationship(
   //   'henrilima',
