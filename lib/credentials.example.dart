3a6a73d9b0a847d7c6f5526be0dc8e9a798a4665
Henrique Lima <henrilima.contactme@gmail.com>
2025-11-06 02:51:19 -0300

    docs: adiciona arquivo de exemplo com variáveis que armazenam dados sensíveis

---
+++ lib/credentials.example.dart
@@ -0,0 +1,8 @@
+abstract class SpotifyCredentials {
+  static String clientId = '// spotify client ID';
+  static String clientSecret = '// spotify Secret Client';
+}
+
+abstract class Credentials {
+  static String storageKey = '// key';
+}
